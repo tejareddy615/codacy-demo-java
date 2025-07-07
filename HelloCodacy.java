3cc14db8b10a58e41d0f2fe858c8522e44573da0
tejareddy615 <tejareddy615@gmail.com>
2025-07-07 15:18:12 -0500

    Introduce intentional issues for Codacy demo

--- HelloCodacy.java
+++ HelloCodacy.java
@@ -1,5 +1,26 @@
+import java.util.*;
+
 public class HelloCodacy {
+
     public static void main(String[] args) {
+        int unusedVariable = 5; // Codacy should flag this
         System.out.println("Hello from Codacy Demo!");
+
+        // Bad practice: empty if block
+        if (true) {
+        }
+
+        // Magic number usage
+        int result = add(10, 42); 
+        System.out.println("Result: " + result);
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    // Unused method
+    public static void doNothing() {
+        // does nothing
     }
 }
